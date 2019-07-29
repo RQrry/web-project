bba6abe2cf446362cf6097911b326fda22143977
RQrry <Naliduo1221@outlook.com>
2019-07-29 18:37:01 +0800

    thinking-in-react state

--- React/thinking-in-react/src/ProductWrap.js
+++ React/thinking-in-react/src/ProductWrap.js
@@ -5,6 +5,7 @@ import './ProductWrap.scss'
 class ProductItem extends React.Component {
   render () {
     const item = this.props.item;
+
     return (
       <div className="product-item">
         <div className={item.stocked ? "" : "name"}>{item.name}</div>
@@ -18,6 +19,7 @@ class ProductItem extends React.Component {
 class ProductTitle extends React.Component {
   render () {
     const category = this.props.category;
+
     return (
       <div className="product-title">
         {category}
@@ -29,15 +31,27 @@ class ProductTitle extends React.Component {
 // ProductList 产品列表
 class ProductList extends React.Component {
   render () {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
     const item = [];
     let title;
+
     this.props.products.forEach(product => {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+
       if (product.category !== title) {
         item.push(<ProductTitle category={product.category} key={product.category} />);
       }
       item.push(<ProductItem item={product} key={product.name} />);
       title = product.category;
     });
+
     return (
       <div className="product-list">
         <div className="title">
@@ -52,12 +66,38 @@ class ProductList extends React.Component {
 
 // SearchTool 搜索
 class SearchTool extends React.Component {
+  constructor (props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange (e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange (e) {
+    this.props.onInStockChange(e.target.checked);
+  }
+
   render () {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
     return (
       <div className="search-tool">
-        <input type="text" placeholder="Search..." />
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+          onChange={this.handleFilterTextChange}
+        />
         <p>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={inStockOnly}
+            onChange={this.handleInStockChange}
+          />
           Only show products in stock
         </p>
       </div>
@@ -67,11 +107,42 @@ class SearchTool extends React.Component {
 
 // ProductWrap 组件包裹框
 class ProductWrap extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange (filterText) {
+    this.setState({
+      filterText: filterText
+    })
+  }
+
+  handleInStockChange (inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    })
+  }
+ 
   render () {
     return (
       <div className="product-wrap">
-        <SearchTool />
-        <ProductList products={this.props.products} />
+        <SearchTool
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductList
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
