fcf96e55d98e0997651bdac232e9597a17e3091f
邱睿 <rqrry@RQrry-MacBook-Pro.local>
2019-08-18 20:39:38 +0800

    banner

--- javascript/banner/js/index.js
+++ javascript/banner/js/index.js
@@ -1,13 +1,22 @@
 // 轮播图插件
-function createBanner(bannerDom, imgLists) {
+// params
+// bannerDom 图片区域    imgLists 图片列表    buttons 左右切换按钮
+function createBanner(bannerDom, imgLists, buttons) {
   if (!imgLists || imgLists.length === 0) {
     return;
   }
 
   // 初始化 变量
+  const WIDTH = bannerDom.offsetWidth;
+  const HEIGHT = bannerDom.offsetHeight;
   const len = imgLists.length;
   let imgArea = document.createElement('div');
   let controlArea = document.createElement('div');
+  let prev = document.createElement('img');
+  let next = document.createElement('img');
+  let curIndex = 0;
+  let timer = null;
+  let speed = 3000;
 
   // 初始化 bannerDom
   bannerDom.innerHTML = '';
@@ -19,6 +28,55 @@ function createBanner(bannerDom, imgLists) {
   createControlArea();
   // 初始化 控制按钮
   createButton();
+  // 初始化 显示第一张图片
+  show(0);
+  // 自动切换图片
+  autoChange();
+  
+  // 鼠标移入停止自动切换
+  imgArea.addEventListener('mouseenter', function () {
+    clearInterval(timer);
+    timer = null;
+    prev.style.opacity = '0.4';
+    next.style.opacity = '0.4';
+  });
+  // 鼠标移出
+  imgArea.addEventListener('mouseleave', function () {
+    autoChange();
+    prev.style.opacity = '0';
+    next.style.opacity = '0';
+  });
+  // 左右切换按钮
+  prev.addEventListener('mouseenter', function () {
+    prev.style.opacity = '0.8';
+    next.style.opacity = '0.8';
+  });
+  prev.addEventListener('mouseleave', function () {
+    prev.style.opacity = '0.4';
+    next.style.opacity = '0.4';
+  });
+  prev.addEventListener('click', function () {
+    if (curIndex === 0) {
+      show(len - 1);
+    } else {
+      show(curIndex - 1);
+    }
+  });
+  next.addEventListener('mouseenter', function () {
+    prev.style.opacity = '0.8';
+    next.style.opacity = '0.8';
+  });
+  next.addEventListener('mouseleave', function () {
+    prev.style.opacity = '0.4';
+    next.style.opacity = '0.4';
+  });
+  next.addEventListener('click', function () {
+    if (curIndex === len - 1) {
+      show(0);
+    } else {
+      show(curIndex + 1);
+    }
+  });
 
   // 创建图片区域
   function createImgArea() {
@@ -30,8 +88,13 @@ function createBanner(bannerDom, imgLists) {
       let img = document.createElement('img');
       img.src = imgLists[i].src;
       img.style.height = '100%';
+      img.style.position = 'absolute';
+      img.style.left = -100 * i;
       img.style.cursor = 'pointer';
-      img.addEventListener('click', function() {
+      img.style.opacity = '0';
+      img.style.zIndex = '0';
+      img.style.transition = 'opacity 0.8s';
+      img.addEventListener('click', function () {
         if (imgLists[i].link) {
           window.open(imgLists[i].link);
         }
@@ -42,8 +105,10 @@ function createBanner(bannerDom, imgLists) {
 
   // 创建控制区域
   function createControlArea() {
+    controlArea.style.position = 'relative';
     controlArea.style.textAlign = 'center';
     controlArea.style.marginTop = '-30px';
+    controlArea.style.zIndex = '2';
     for (let i=0; i<len; i++) {
       let span = document.createElement('span');
       span.style.display = 'inline-block';
@@ -53,20 +118,67 @@ function createBanner(bannerDom, imgLists) {
       span.style.borderRadius = '50%';
       span.style.backgroundColor = 'lightgray';
       span.style.margin = '0 5px';
+      span.addEventListener('click', function () {
+        show(i);
+      });
       controlArea.appendChild(span);
     }
   }
 
   // 创建控制按钮
   function createButton() {
-    let prev = document.createElement('div');
-    let next = document.createElement('div');
+    prev.src = buttons[0];
+    next.src = buttons[1];
     prev.style.position = 'absolute';
-    prev.style.width = '20px';
-    prev.style.height = '20px';
-    prev.style.backgroundImage = ''
+    prev.style.width = '40px';
+    prev.style.height = '40px';
+    prev.style.left = 0;
+    prev.style.top = (HEIGHT - 40) / 2 + 'px';
+    prev.style.cursor = 'pointer';
+    prev.style.opacity = '0';
+    prev.style.zIndex = '2';
+    prev.style.transition = 'opacity 0.8s';
     next.style.position = 'absolute';
+    next.style.width = '40px';
+    next.style.height = '40px';
+    next.style.right = 0;
+    next.style.top = (HEIGHT - 40) / 2 + 'px';
+    next.style.cursor = 'pointer';
+    next.style.opacity = '0';
+    next.style.zIndex = '2';
+    next.style.transition = 'opacity 0.8s';
     imgArea.appendChild(prev);
     imgArea.appendChild(next);
+  }
+
+  // 根据指定的索引切换图片
+  function show(index) {
+    curIndex = index;
+    for (let i=0; i<len; i++) {
+      const el = controlArea.children[i];
+      if (i === curIndex) {
+        el.style.backgroundColor = 'darkgoldenrod';
+        imgArea.children[i].style.opacity = '1';
+        imgArea.children[i].style.zIndex = '1';
+      } else {
+        el.style.backgroundColor = 'lightgray';
+        imgArea.children[i].style.zIndex = '0';        
+        imgArea.children[i].style.opacity = '0';
+      }
+    }
+  }
+
+  // 自动切换
+  function autoChange() {
+    if (timer) {
+      return;
+    }
+    timer = setInterval(function () {
+      if (curIndex === len - 1) {
+        show(0);
+      } else {
+        show(curIndex + 1);
+      }
+    }, speed);
   }
 }
